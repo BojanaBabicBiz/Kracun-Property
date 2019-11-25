bb72feafbf5dd841482e160c2f683cd6fca410c7
nikola djordjevic <nikola.dj.87@gmail.com>
2019-11-25 09:46:56 +0100

    text tweek

--- src/component/AboutPage.js
+++ src/component/AboutPage.js
@@ -13,22 +13,16 @@ export default function AboutPage() {
                         <h1 className="about-title">
                             Ko Smo Mi
                         </h1>
-                        <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cumque, neque illo fugit
-                        ut porro
-                        tempore quasi beatae, pariatur quidem eligendi repellat quibusdam sint quos voluptatum
-                        magni
-                        perferendis inventore exercitationem tempore quasi beatae, pariatur quidem eligendi
-                        repellat
-                        quibusdam sint quos voluptatum magni
-                        perferendis inventore exercitationem tempore quasi beatae, pariatur quidem eligendi
-                        repellat
-                        quibusdam sint quos voluptatum magni
-                        perferendis inventore exercitationem.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat cumque, neque illo fugit
-                            ut porro
-                            tempore quasi beatae, pariatur quidem eligendi repellat quibusdam sint quos voluptatum
-                            magni
-                        perferendis inventore exercitationem.</p>
+                        <p >„Kračun“ je jedna je od prvih agencija za nekretnine u Vojvodini. Sa dokazanim iskustvom i uspehom u radu sa nekretninama i zadovoljnim klijentima na našoj strani, svoje usluge sada nudimo na teritoriji cele Republike Srbije sa mogućnošću saradnje i sa stranim agencijama.
+
+                        </p>
+
+                        <p>Naša misija je da pronađemo nekretninu koja u svemu odgovara Vašim željama i mogućnostima, a najbolji pokazatelj da smo u tome uspeli je poverenje koje smo stekli.</p>
+                        <p>Tim koji će ostvariti sve Vaše snove poseduje znanje i iskustvo ne samo u radu sa nekretninama, već i u saradnji sa specijalizovanim advokatskim kancelarijama pomoći ćemo Vam da stepen pravne sigurnosti bude na najvišem nivou.</p>
+
+                        <h1 className="about-subtitle">
+                            MISLIMO GLOBALNO DELUJEMO LOKALNO
+                        </h1>
                     </div>
                 </div>
             </div>
